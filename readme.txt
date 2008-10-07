d6eefee7aa8ac30a3e1446764149c443af97f1f1
cyberhobo <cyberhobo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-07 02:07:58 +0000

    A star is born, first Tally Graph commit.
    git-svn-id: https://plugins.svn.wordpress.org/tally-graph/trunk@67696 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,200 @@
+=== Tally Graph ===
+Contributors: cyberhobo
+Donate link: http://www.cyberhobo.net/downloads/wordpress-tally-graph-plugin/
+Tags: graphs, charts, google, mashup, visualization, custom fields
+Requires at least: 2.5.1
+Tested up to: 2.6
+Stable tag: 0.1
+
+Add Google charts and graphs to your WordPress site based on tallies of any
+numeric custom field over time. Visualize progress toward any goal. 
+
+== Description ==
+
+The ability to see progress over time is a great motivator, whether the goal
+is related to athletic training, profits, environmental footprint, weight loss,
+or any topic you care about.  Combine WordPress with the Google Chart API,
+and you get a powerful way to track and visualize your data over time. 
+
+As an example, here is one of my goals for Tally Graph:
+
+*   It will generate enough donations to allow me to develop it further. Even
+$100/month would allow me to turn down a little bit of less interesting work. 
+
+To see Tally Graph in action visualizing progress toward this goal, 
+visit the [donation page][].
+
+[donation page]: http://www.cyberhobo.net/downloads/wordpress-tally-graph-plugin/
+
+![Donation Graph](http://cyberhobo.net/wp-content/plugins/tally-graph/translate.php?key=tally_graph_donations)
+
+= Features =
+
+*   Tallies data from any numeric value you enter under "Custom Field" in the WordPress post editor.
+*   Provides daily, weekly, monthly, or yearly tallies.
+*   You can make basic use of Tally Graph without any knowlege of the Google Chart API, but you can 
+also use nearly any [Google Chart API parameters][gapi].
+
+[gapi]: http://code.google.com/apis/chart/
+
+== Installation ==
+
+Installation should be the same as any WordPress hosted plugin:
+
+1. Click the download button and save the `tally-graph.zip` file.
+2. Expand the ZIP file to create the `tally-graph` directory.
+3. Upload the `tally-graph` directory and all the files in it to the 
+`wp-content/plugins` directory on your server.
+4. Activate the plugin in the "Plugins" administration tab of WordPress.
+
+== Frequently Asked Questions ==
+
+= Do I have to know about this Google API thing, or anything else techy? =
+
+You can get by with very little techyness, just WordPress custom fields
+and shortcodes. They're really not bad - look over the Usage section.
+
+If you do want to get adventurous, you can have fancier charts in more places.
+
+= Can I skip learning to use Tally Graph and just hire you to put charts on my site? =
+
+Sure, just send email to <cyberhobo@cyberhobo.net>.
+
+== Usage ==
+
+You'll want to do two things to use Tally Graph:
+
+*   *Enter Data* - put the numbers you want to track in a WordPress custom field.
+*   *Visualize Data* - sum up your numbers over time in a Google Chart.
+
+= Enter Data =
+
+Tally Graph looks in WordPress [custom fields][] for data to pass on to the
+Google Chart API. As indicated by those instructions, a custom field consists
+of a key and value. You'll make up the key. The value must be some kind of
+number. In the next step you'll use the key name to tell Tally Graph which
+custom field data to use.
+
+[custom fields]: http://codex.wordpress.org/Using_Custom_Fields
+
+If you want to enter data without publishing a post, just put the custom fields 
+on a dummy post and check "Keep this post private". You may still want to edit the
+post date.
+
+= Visualize Data =
+
+Say you have a bunch of posts with the custom field key "Marbles Lost". You may
+feel like you've been losing your marbles faster recently, but you're not sure,
+so you write a new post (or a page) containing this shortcode:
+
+`[tally_graph key="Marbles Lost"]`
+
+The Tally Graph plugin will replace that shortcode with a bar chart of how many
+marbles you've lost each month for the past six months. Those are default settings
+you can change with some more parameters. 
+
+`[tally_graph key="Marbles Lost" tally_interval="day" interval_count="14" 
+to_date="2008-05-01" chs="300x220" chtt="Marbles Lost"]`
+
+That shortcode results in a 300 pixel wide, 220 pixel high graph of your marbles 
+lost in the 14 days prior to May 1st, 2008.
+
+Shortcodes are great in post and page content, but if you want a graph somewhere
+else on your site, like in a sidebar, you'll need a [template tag][]. Aside from the 
+different format, it works the same:
+
+[template tag]: http://codex.wordpress.org/Template_Tags 
+
+`<?php echo tally_graph('key=Marbles Lost&tally_interval=week&interval_count=4'); ?>`
+
+That makes a nice sidebar graph of marbles lost over 4 weeks, up to the date of 
+the last post displayed.
+
+== Screenshots ==
+
+1. Some examples of custom fields attached to a post. They'll be tallied by the
+   post date.
+2. Some different kinds tally graphs in a page.
+3. Some monthly tally graphs in a sidebar.
+
+== Tag Reference ==
+
+Read the Usage section first. 
+
+You type a tag directly into a post or page using WordPress [shortcode format][]. 
+To put a tag in a theme template, use [template tag with querystring parameters][1].
+Both formats take the same parameters listed below.
+
+[shortcode format]: http://codex.wordpress.org/Shortcode_API
+[1]: http://codex.wordpress.org/Template_Tags/How_to_Pass_Tag_Parameters#Tags_with_query-string-style_parameters
+
+= tally_graph =
+
+This tag is replaced with an image created with the Google Chart API.
+
+Shortcode: `[tally_graph ...]`
+
+Template Tag: `<?php echo tally_graph(...); ?>`
+
+Parameters:
+
+*   **key** - Required. 
+
+    The key name of the custom field to use for the graph.  Multiple keys can
+    be included, separated by a comma.
+
+*   *tally_interval* 
+
+    Valid values: `day`, `week`, `month`, or `year`.  Default is `month`.  
+
+    This is the interval of time over which the custom field values are 
+    tallied.
+
+*   *interval_count* 
+
+    Default is `6`. 
+
+    This is the number of intervals to include in the graph.
+
+*   *to_date* 
+
+    Valid values include several date formats, like `2007-10-31`, 
+    `October 31, 2007`, `today`, or `yesterday`. Default is the date of the 
+    most recent post displayed. 
+
+    The graph is constructed backward in time from this date. 
+
+*   *no-cache* 
+
+    Valid values: `true` or `false`. Default is `false`. 
+
+    Setting to `true` forces data to be queried with every page hit, making
+    sure recent updates are included.
+
+*   *chs* 
+
+    Default is `200x200`. 
+
+    This is a [Google Chart API][gapi] parameter, the chart size in pixels. 
+
+*   *cht* 
+
+    Default is `bvs`. 
+
+    This is a [Google Chart API][gapi] parameter, the chart type. `bvs` 
+    is a vertical bar chart.
+
+*   Any other [Google Chart API parameters][gapi] are passed along, so you 
+    can go nuts with all the options. You'll probably use at least `chtt`, 
+    the chart title.
+
+[gapi]: http://code.google.com/apis/chart/
+
+= tally_graph_url =
+
+If you want to create your own image tag in a template, this tag will give 
+you only the URL for the chart.
+
+Template Tag: `<?php echo tally_graph_url(...); ?>`
+
+Parameters are the same as the `tally_graph` tag.
---
+++ tally-graph.php
@@ -0,0 +1,212 @@
+<?php /*
+Plugin Name: Tally Graph
+Plugin URI: http://wordpress.org/extend/plugins/tally-graph/
+Description: Add Google charts and graphs to your WordPress site based on tallies of any numeric custom field over time. Visualize progress toward any goal by day, week, month, or year.
+Version: 0.1
+Author: Dylan Kuhn
+Author URI: http://www.cyberhobo.net/
+Minimum WordPress Version Required: 2.5.1
+*/
+
+/*
+Copyright (c) 2005-2007 Dylan Kuhn
+
+This program is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation;
+either version 2 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more
+details.
+*/
+
+	
+function tally_graph($atts) {
+	return '<img src="'.tally_graph_url($atts).'" alt="'.$atts['key'].'" />';
+}
+
+function tally_graph_url($atts) {
+	global $post;
+
+	$atts = wp_parse_args($atts);
+	$defaults = array('interval_count' => '6', 'chs' => '200x200', 'cht' => 'bvs');
+	$atts = array_merge($defaults, $atts);
+	if (!isset($atts['key'])) return 'Tally Graph: required parameter "key" is missing.';
+	
+	// Extract non-chart variables from attributes
+	$keys = split(',',$atts['key']); 
+	unset($atts['key']);
+	$use_cache = true;
+	if (isset($atts['no-cache'])) {
+		$use_cache = false;
+		unset($atts['no-cache']);
+	}
+	if (isset($atts['to_date'])) {
+		$end_time = strtotime($atts['to_date']);
+		if (!$end_time) {
+			return 'Tally Graph: couldn\'t read the to_date ' . $atts['to_date'];
+		}
+		unset($atts['to_date']);
+	} else if ($post) {
+		$end_time = strtotime($post->post_date);
+	} else {
+		$end_time = time();
+	}
+	if (isset($atts['tally_interval'])) {
+		$tally_interval = $atts['tally_interval'];
+		unset($atts['tally_interval']);
+	} else {
+		$tally_interval = 'month';
+	}
+	$interval_count = $atts['interval_count'];
+	unset($atts['interval_count']);
+	
+	list($index_gnu_format, $index_mysql_format, $first_day_suffix) = tally_graph_interval_settings($tally_interval);
+
+	// Always start on the first day of the starting interval
+	$start_time = strtotime('-'.$interval_count.' '.$tally_interval,$end_time);
+	$next_date_prefix = date($index_gnu_format,strtotime('+1 '.$tally_interval,$start_time));
+	$start_time = strtotime($next_date_prefix.$first_day_suffix);
+	// Always end on the first day after the ending interval
+	$next_date_prefix = date($index_gnu_format,strtotime('+1 '.$tally_interval,$end_time));
+	$end_time = strtotime($next_date_prefix.$first_day_suffix);
+
+	// Return cached URL if available
+	if ($use_cache) {
+		$cache_key = 'tally-graph-'.md5($start_time.$end_time.$tally_interval.serialize($atts));
+		$cached_url = wp_cache_get($cache_key);
+		if ($cached_url) {
+			return $cached_url;
+		}
+	}
+
+	// Tally ho
+	$key_counts = array();
+	foreach($keys as $index => $key) {
+		$key_counts[$index] = tally_graph_get_counts($key, $start_time, $end_time, $tally_interval);
+	}
+
+	// Build the chart parameters
+	$chd = 't:';
+	$month_names = array('01' => 'Jan', '02' => 'Feb', '03' => 'Mar', '04' => 'Apr', '05' => 'May',
+		'06' => 'Jun', '07' => 'Jul', '08' => 'Aug', '09' => 'Sep', '10' => 'Oct', '11' => 'Nov', '12' => 'Dec');
+	$first_index = $chd_min = $chd_max = null;
+	foreach($key_counts as $index => $counts) {
+		if (is_null($first_index)) $first_index = $index;
+		if ($index != $first_index) $chd .= '|';
+		$comma = '';
+		foreach($counts as $date_index => $count) {
+			$compare_count = $count;
+			if ($index != $first_index && $atts['cht'] == 'bvs') {
+				$compare_count = $key_counts[$first_index][$date_index] + $count;
+			}
+			if (is_null($chd_min) || $compare_count < $chd_min) $chd_min = $compare_count;
+			if (is_null($chd_max) || $compare_count > $chd_max) $chd_max = $compare_count;
+			$chd .= $comma . $count;
+			$comma = ',';
+			if ($index == $first_index) {
+				$day_label_string .= '|'.substr($date_index,8,2);
+				$week_label_string .= '|'.substr($date_index,5,3);
+				$month = $month_names[substr($date_index,5,2)];
+				if ($month != $last_month) $last_month = $month;
+				else $month = ' ';
+				$month_label_string .= '|'.$month;
+				$year = substr($date_index,0,4);
+				if ($year != $last_year) $last_year = $year;
+				else $year = ' ';
+				$year_label_string .= '|'.$year;
+			}
+		}
+	}
+	// Give nonzero minimum values a 10% pad
+	$pad_min = floor($chd_min - (($chd_max - $chd_min)/10));
+	$chd_min = ($pad_min>0) ? $pad_min : $chd_min;
+	
+	// Set Google chart attributes
+	// chart data
+	$atts['chd'] = $chd;
+	if (!isset($atts['chds'])) {
+		// Provide chart scale
+		$atts['chds'] = $chd_min.','.$chd_max;
+	}
+	if (!isset($atts['chxt'])) {
+		// Provide labels
+		if ($tally_interval == 'year') {
+			$atts['chxt'] = 'y,x';
+			$atts['chxl'] = '1:'.$year_label_string;
+		} else if ($tally_interval == 'week') {
+			$atts['chxt'] = 'y,x,x';
+			$atts['chxl'] = '1:'.$week_label_string.'|2:'.$year_label_string;
+		} else if ($tally_interval == 'day') {
+			$atts['chxt'] = 'y,x,x,x';
+			$atts['chxl'] = '1:'.$day_label_string.'|2:'.$month_label_string.
+				'3:'.$year_label_string;
+		} else {
+			$atts['chxt'] = 'y,x,x';
+			$atts['chxl'] = '1:'.$month_label_string.'|2:'.$year_label_string;
+		}
+	}
+	if (!isset($atts['chxr'])) {
+		// Provide count range labels at index 0
+		$atts['chxr'] = '0,'.$chd_min.','.$chd_max;
+	}
+	$chart_url = 'http://chart.apis.google.com/chart';
+	$separator = '?';
+	foreach($atts as $name => $value) {
+		$chart_url .= $separator.$name.'='.urlencode($value);
+		$separator = '&amp;';
+	}
+	if ($use_cache) {
+		wp_cache_set($cache_key, $chart_url, '', 60 * 60 * 24 * 7); 
+	}
+	return $chart_url;
+}
+
+function tally_graph_get_counts($key, $start_time, $end_time, $interval) {
+	global $wpdb;
+
+	list($gnu_index_format, $mysql_index_format) = tally_graph_interval_settings($interval);
+	$counts = array();
+	for($the_time = $start_time; $the_time < $end_time; $the_time = strtotime('+1 '.$interval,$the_time)) {
+		$counts[date($gnu_index_format,$the_time)] = 0;
+	}
+
+	$query_sql = 'SELECT DATE_FORMAT(p.post_date,\''.$mysql_index_format.'\') AS xdata '.
+		',SUM(pm.meta_value) AS ydata '.
+		'FROM '. $wpdb->posts .' p '. 
+		'JOIN '. $wpdb->postmeta .' pm ON pm.post_id = p.ID '.
+		'WHERE pm.meta_key = \''. $key .'\' '.
+		'AND p.post_date >= \''.date('Y-m-d',$start_time).'\' '.
+		'AND p.post_date < \''.date('Y-m-d',$end_time).'\' '.
+		'GROUP BY DATE_FORMAT(p.post_date,\''.$mysql_index_format.'\') '.
+		'ORDER BY p.post_date';
+	$wpdb->query($query_sql);
+	if ($wpdb->last_result) {
+		$chd_min = $chd_max = $wpdb->last_result[0]->ydata;
+		foreach ($wpdb->last_result as $result) {
+			if ($result->ydata < $chd_min) $chd_min = $result->ydata;
+			else if ($result->ydata > $chd_max) $chd_max = $result->ydata;
+			$counts[$result->xdata] = $result->ydata;
+		}
+	}
+	return $counts;
+}
+
+function tally_graph_interval_settings($interval) {
+	if ($interval == 'year') {
+		return array('Y','%Y','-01-01');
+	} 
+	if ($interval == 'week') {
+		return array('Y-\\WW','%Y-W%v','-1');
+	} 
+	if ($interval == 'day') {
+		return array('Y-m-d','%Y-%m-%d','');
+	} 
+	return array('Y-m','%Y-%m','-01');
+}
+
+add_shortcode('tally_graph','tally_graph');
