b64486684340cca37f583c48cc7911cd3a55d1f3
cyberhobo <cyberhobo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-04 20:14:35 +0000

    Added a "list" chart type that just spits out the list of counts.
    git-svn-id: https://plugins.svn.wordpress.org/tally-graph/trunk@142803 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tally-graph.php
+++ tally-graph.php
@@ -37,7 +37,11 @@ function tally_graph($atts) {
 	if (isset($atts['class'])) {
 		$class = $atts['class'];
 	}
-	return '<img class="'.$class.'" src="'.tally_graph_url($atts).'" alt="'.$atts['key'].'" />';
+	if ( isset( $atts['cht'] ) && 'list' == $atts['cht'] ) {
+		return '<span class="' . $class . '">' . tally_graph_url( $atts ) . '</span>';
+	} else {
+		return '<img class="'.$class.'" src="'.tally_graph_url($atts).'" alt="'.$atts['key'].'" />';
+	}
 }
 
 function tally_graph_week2date($year, $week, $weekday=6) {
@@ -122,9 +126,9 @@ function tally_graph_url($atts) {
 	}
 
 	// Build the chart parameters
-	$chd = 't:';
 	$month_names = array('01' => 'Jan', '02' => 'Feb', '03' => 'Mar', '04' => 'Apr', '05' => 'May',
 		'06' => 'Jun', '07' => 'Jul', '08' => 'Aug', '09' => 'Sep', '10' => 'Oct', '11' => 'Nov', '12' => 'Dec');
+	$chd = $day_label_string = $week_label_string = $month_label_string = $year_label_string = $last_month = $last_year = '';
 	$first_index = $chd_min = $chd_max = null;
 	foreach($key_counts as $index => $counts) {
 		if (is_null($first_index)) $first_index = $index;
@@ -142,7 +146,7 @@ function tally_graph_url($atts) {
 			if ($index == $first_index) {
 				$day_label_string .= '|'.substr($date_index,8,2);
 				$week_label_string .= '|'.substr($date_index,5,3);
-				$month = $month_names[substr($date_index,5,2)];
+				$month = isset( $month_names[substr($date_index,5,2)] ) ? $month_names[substr($date_index,5,2)] : '' ;
 				if ($month != $last_month) $last_month = $month;
 				else $month = ' ';
 				$month_label_string .= '|'.$month;
@@ -153,13 +157,18 @@ function tally_graph_url($atts) {
 			}
 		}
 	}
+	// Return just the list if requested
+	if ( 'list' == $atts['cht'] ) {
+		return $chd;
+	}
+
 	// Give nonzero minimum values a 10% pad
 	$pad_min = floor($chd_min - (($chd_max - $chd_min)/10));
 	$chd_min = ($pad_min>0) ? $pad_min : $chd_min;
 	
 	// Set Google chart attributes
 	// chart data
-	$atts['chd'] = $chd;
+	$atts['chd'] = 't:' . $chd;
 	if (!isset($atts['chds'])) {
 		// Provide chart scale
 		$atts['chds'] = $chd_min.','.$chd_max;
